53f6dfc74258c3e37cf9af1afcfd30c557a09456
RESQ Chain <PaulH-RESQ>
2019-04-02 09:47:16 +0200

    Logo Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,8 +39,8 @@
   <nav class="navbar nav-custom navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
           <a class="navbar-brand" href="index.php">
-              <img src="/assets/imgs/logo/cs_bw_sm.png">
-              Connectivity Services
+              <img src="/assets/imgs/logo/TFP-Logo.png">
+              The Fibre Provider
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
                   aria-expanded="false" aria-label="Toggle navigation">
